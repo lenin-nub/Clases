546f4153133a8bffe1b1a77d0bf61060fd69bf2f
lenin-nub <paullenin@hotmial.es>
2020-06-11 19:21:02 -0500

    DESARROLLO

---
+++ Clases.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio7.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class EJERCICIO71 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner objeto = new Scanner(System.in);
+        System.out.println("Implementación de un Multimodelo Neuronal Jerarquico para Identificación y Control de Sistemas Mecanicos");
+        System.out.println("\n   Ingrese clase padre :");
+        String clp =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 1 :");
+        String atributo1 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 2 :");
+        String atributo2 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 3 :");
+        String atributo3 =objeto.nextLine();
+
+        System.out.println("\n   Ingrese Servicios :");
+        System.out.println("\n   Ingrese Servicio 1 :");
+        String servicio1 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 2 :");
+        String servicio2 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 3 :");
+        String servicio3 =objeto.nextLine();
+
+
+        System.out.println("\n   Ingrese Clase Hijos :");
+        System.out.println("\n   Ingrese Clase Hijo :");
+        String hijo1 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 1 :");
+        String atributo4 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 2 :");
+        String atributo5 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 3 :");
+        String atributo6 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 4 :");
+        String atributo7 =objeto.nextLine();
+
+        System.out.println("\n   Ingrese Servicios :");
+        System.out.println("\n   Ingrese Servicio 1 :");
+        String servicio4 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 2 :");
+        String servicio5 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 3 :");
+        String servicio6 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 4 :");
+        String servicio7 =objeto.nextLine();
+
+        System.out.println("\n   Ingrese Clase Hijo :");
+        String hijo2 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 1 :");
+        String atributo8 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 2 :");
+        String atributo9 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 3 :");
+        String atributo10 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 4 :");
+        String atriburo11 =objeto.nextLine();
+
+        System.out.println("\n   Ingrese Servicios :");
+        System.out.println("\n   Ingrese Servicio 1 :");
+        String servicio8 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 2 :");
+        String servicio9 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 3 :");
+        String servicio10 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 4 :");
+        String servicio11 =objeto.nextLine();
+    }
+
+}
